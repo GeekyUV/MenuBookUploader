87c173d488391732d8f8fdc77fc75f64a1d34362
GeekyUV <singhyuvi973@gmail.com>
2023-09-02 14:51:46 +0530

    error handling

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,33 +1,44 @@
 class DishesController < ApplicationController
-    def index
-        @dishes = Dish.all
-      end
-    
-      def import
-        csv_file = params[:file]
-        notice_message = "No CSV file was uploaded." # Default message
-    
-        if csv_file.present?
-          if csv_file.size <= 5.kilobytes
-            CsvProcessingWorker.new.perform(csv_file.path)
-            notice_message = "CSV file imported successfully!"
-          else
-            CsvProcessingWorker.perform_async(csv_file.path)
-            notice_message = "CSV file import is in progress!"
-          end
+  def index
+    @dishes = Dish.all
+  end
+
+  def import
+    csv_file = params[:file]
+    notice_message = "No CSV file was uploaded."
+
+    begin
+      if csv_file.present?
+        if csv_file.size <= 5.kilobytes
+          CsvProcessingWorker.new.perform(csv_file.path)
+          notice_message = "CSV file imported successfully!"
+        else
+          CsvProcessingWorker.perform_async(csv_file.path)
+          notice_message = "CSV file import is in progress!"
         end
-    
-        redirect_to root_url, notice: notice_message
       end
-    
-      def show
-        @dishes = Dish.all
-      end
-    
-      def destroy_all
-        Dish.destroy_all
-        redirect_to dish_path, notice: 'All dishes deleted successfully'
-      end
-    
-    
+    rescue CSV::MalformedCSVError => e
+      flash[:alert] = "Malformed CSV file: #{e.message}"
+    rescue StandardError => e
+      flash[:alert] = "An error occurred during import: #{e.message}"
+    end
+
+    redirect_to root_url, notice: notice_message
+  end
+
+  def show
+    @dishes = Dish.all
+  end
+
+  def destroy_all
+    begin
+      Dish.destroy_all
+      flash[:notice] = 'All dishes deleted successfully'
+    rescue StandardError => e
+      flash[:alert] = "An error occurred while deleting dishes: #{e.message}"
+    end
+
+    redirect_to dish_path
+  end
+
 end
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,14 +1,28 @@
 require 'csv'
+
 class Dish < ApplicationRecord
-    def self.import(file)
-        CSV.foreach(file.path, headers: true).in_batches(of: 1000) do |batch|
-          Dish.transaction do
-            batch.each do |row|
-              dish_hash = row.to_hash
-              dish = Dish.find_or_initialize_by(dish_name: dish_hash['dish_name'])
-              dish.update(dish_hash)
+  def self.import(file)
+    begin
+      CSV.foreach(file.path, headers: true).in_batches(of: 1000) do |batch|
+        Dish.transaction do
+          batch.each do |row|
+            dish_hash = row.to_hash
+            dish = Dish.find_or_initialize_by(dish_name: dish_hash['dish_name'])
+
+            dish.update(dish_hash)
+
+            if dish.errors.any?
+              errors = dish.errors.full_messages.join(', ')
+              raise "Error importing data: #{errors}"
             end
+
           end
         end
       end
+    rescue CSV::MalformedCSVError => e
+      raise "Malformed CSV file: #{e.message}"
+    rescue StandardError => e
+      raise "An error occurred during import: #{e.message}"
+    end
+  end
 end
